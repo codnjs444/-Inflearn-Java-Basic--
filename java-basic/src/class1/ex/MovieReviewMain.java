86e36759ce6360e97045b4d6e304994d48a5fae2
codnjs444 <81910499+codnjs444@users.noreply.github.com>
2024-11-04 13:38:48 +0900

    24.11.04 : 진행중

---
+++ java-basic/src/class1/ex/MovieReviewMain.java
@@ -0,0 +1,9 @@
+package class1.ex;
+
+public class MovieReviewMain {
+    public static void main(String[] args) {
+        MovieReview[] movieReviews = new MovieReview[2];
+        movieReviews[0].review = "11";
+        movieReviews[0].review = "1111";
+    }
+}
