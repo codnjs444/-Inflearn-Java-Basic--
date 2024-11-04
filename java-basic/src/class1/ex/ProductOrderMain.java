87d8a3aa97ca0d020eeb47084a812d2fb6e5d7ff
codnjs444 <81910499+codnjs444@users.noreply.github.com>
2024-11-04 14:25:30 +0900

    24.11.04 : 문제 연습 풀이

---
+++ java-basic/src/class1/ex/ProductOrderMain.java
@@ -0,0 +1,33 @@
+package class1.ex;
+
+public class ProductOrderMain {
+    public static void main(String[] args) {
+        ProductOrder[] productOrders = new ProductOrder[3];
+
+        ProductOrder menu1 = new ProductOrder();
+        menu1.productName = "두부";
+        menu1.price = 3000;
+        menu1.quantity = 3;
+        productOrders[0] = menu1;
+
+        ProductOrder menu2 = new ProductOrder();
+        menu2.productName = "토마토";
+        menu2.price = 1000;
+        menu2.quantity = 4;
+        productOrders[1] = menu2;
+
+        ProductOrder menu3 = new ProductOrder();
+        menu3.productName = "감자";
+        menu3.price = 6000;
+        menu3.quantity = 5;
+        productOrders[2] = menu3;
+
+        int totalPrice = 0;
+        for (ProductOrder productOrder : productOrders) {
+            totalPrice += productOrder.price * productOrder.quantity;
+            System.out.println(productOrder.productName + productOrder.price + productOrder.quantity);
+        }
+        System.out.println("총 가격은 = " + totalPrice);
+
+    }
+}
