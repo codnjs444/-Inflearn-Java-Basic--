724ab45b571e002fab171d81c6d359e96aefa706
codnjs444 <81910499+codnjs444@users.noreply.github.com>
2024-11-09 16:45:08 +0900

    24.11.09 : final

---
+++ java-basic/src/final1/Constant.java
@@ -0,0 +1,9 @@
+package final1;
+
+public class Constant {
+    public static final double Pi = 3.14;
+
+    public static final int HOURS_IN_DAY = 24;
+
+    public static final int MAX_USERS = 1000;
+}
---
+++ java-basic/src/final1/FinalLocalMain.java
@@ -0,0 +1,16 @@
+package final1;
+
+public class FinalLocalMain {
+    public static void main(String[] args) {
+        final int data1;
+        data1 = 10;
+//        data1 = 20;
+
+        final int data2 = 10;
+
+    }
+
+    static void method(final int parameter) {
+
+    }
+}
