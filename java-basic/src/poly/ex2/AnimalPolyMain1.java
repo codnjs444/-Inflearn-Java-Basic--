6684e218928023820ae91efb1693d53f80d361fc
codnjs444 <81910499+codnjs444@users.noreply.github.com>
2024-11-15 15:47:20 +0900

    24.11.15 : 다형성 + overrinding (공동체 효과)

---
+++ java-basic/src/poly/ex2/AnimalPolyMain1.java
@@ -0,0 +1,23 @@
+package poly.ex2;
+
+import poly.ex2.Caw;
+
+public class AnimalPolyMain1 {
+    public static void main(String[] args) {
+        Dog dog = new Dog();
+        Cat cat = new Cat();
+        Caw caw = new Caw();
+        Dock dock = new Dock();
+
+        soundAnimal(dog);
+        soundAnimal(cat);
+        soundAnimal(caw);
+        soundAnimal(dock);
+    }
+
+    private static void soundAnimal(Animal animal) {
+        System.out.println("동물 소리 사운드 시작");
+        animal.sound();
+        System.out.println("동물 소리 사운드 종료");
+    }
+}
